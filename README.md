2b472b0ccd81bc4c63be6ae0975a2105f7b6389b
Luke Street <luke.street@encounterpc.com>
2023-09-07 16:29:18 -0400

    Cleanup & add README, LICENSE, CI config

---
+++ README.md
@@ -0,0 +1,54 @@
+# sjiswrap [![Build Status]][actions]
+
+[Build Status]: https://github.com/encounter/sjiswrap/actions/workflows/build.yml/badge.svg
+[actions]: https://github.com/encounter/sjiswrap/actions
+
+UTF-8 to Shift JIS wrapper for old 32-bit Windows compilers.
+
+When the wrapped executable reads a text file, it will be encoded from UTF-8 to Shift JIS on the fly.
+
+Encoded file extensions:
+- `.c`
+- `.cc`
+- `.cp`
+- `.cpp`
+- `.cxx`
+- `.h`
+- `.hh`
+- `.hp`
+- `.hpp`
+- `.hxx`
+
+## Usage
+
+Download the latest release from [here](https://github.com/encounter/sjiswrap/releases).
+
+```shell
+$ sjiswrap.exe <exe> [args...]
+```
+
+## Building
+
+```shell
+$ cargo build --target i686-pc-windows-msvc --release
+```
+
+For smaller binaries:
+
+```shell
+$ cargo +nightly build -Z build-std=std,panic_abort --target i686-pc-windows-msvc --release
+```
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as
+defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#![allow(non_snake_case)]
-
+#![allow(non_snake_case, clippy::let_and_return)]
 use std::{
     borrow::Cow,
     cmp::min,
@@ -13,15 +12,15 @@ use std::{
     sync::Mutex,
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use encoding_rs::SHIFT_JIS;
 use lazy_static::lazy_static;
 use windows::{
-    core::PCSTR,
+    core::{PCSTR, PCWSTR},
     Win32::{
         Foundation::{
             CloseHandle, GetLastError, SetLastError, BOOL, GENERIC_ACCESS_RIGHTS, GENERIC_READ,
-            HANDLE,
+            HANDLE, INVALID_HANDLE_VALUE,
         },
         Security::SECURITY_ATTRIBUTES,
         Storage::FileSystem::{
@@ -33,29 +32,91 @@ use windows::{
     },
 };
 
+/// Whether to hook and encode a file.
+fn is_text_file(path: &str) -> bool {
+    path.ends_with(".c")
+        || path.ends_with(".cc")
+        || path.ends_with(".cp")
+        || path.ends_with(".cpp")
+        || path.ends_with(".cxx")
+        || path.ends_with(".h")
+        || path.ends_with(".hh")
+        || path.ends_with(".hp")
+        || path.ends_with(".hpp")
+        || path.ends_with(".hxx")
+}
+
+macro_rules! debug_println {
+    ($($arg:tt)*) => {
+        #[cfg(feature = "debug")]
+        {
+            // Writing to console will overwrite LastError
+            let err = unsafe { GetLastError() };
+            eprintln!($($arg)*);
+            unsafe { SetLastError(err) };
+        }
+    };
+}
+
+fn main() -> Result<()> {
+    let args: Vec<OsString> = std::env::args_os().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <exe>", args[0].to_string_lossy());
+        exit(1);
+    }
+
+    let path = PathBuf::from(&args[1]);
+    let parent = CString::new(
+        path.parent()
+            .context("Failed to get executable parent directory")?
+            .to_string_lossy()
+            .as_ref(),
+    )
+    .unwrap();
+    let parent =
+        get_full_path(&parent).expect("Failed to get absolute executable parent directory");
+    unsafe { SetDllDirectoryA(PCSTR(parent.as_ptr() as *const u8)) }
+        .ok()
+        .context("SetDllDirectoryA() failed")?;
+    debug_println!("SetDllDirectoryA({:?})", parent.to_string_lossy());
+
+    let mut buf = Vec::new();
+    File::open(&path)
+        .with_context(|| format!("Failed to open executable: '{}'", path.display()))?
+        .read_to_end(&mut buf)
+        .with_context(|| format!("Failed to read executable: '{}'", path.display()))?;
+
+    let mut hooks = HashMap::new();
+    hooks.insert("kernel32.dll!GetCommandLineA".into(), hook_GetCommandLineA as *const c_void);
+    hooks.insert("kernel32.dll!GetCommandLineW".into(), hook_GetCommandLineW as *const c_void);
+    hooks.insert("kernel32.dll!CreateFileA".into(), hook_CreateFileA as *const c_void);
+    hooks.insert("kernel32.dll!CreateFileW".into(), hook_CreateFileW as *const c_void);
+    hooks.insert("kernel32.dll!GetFileSize".into(), hook_GetFileSize as *const c_void);
+    hooks.insert("kernel32.dll!CloseHandle".into(), hook_CloseHandle as *const c_void);
+    hooks.insert("kernel32.dll!ReadFile".into(), hook_ReadFile as *const c_void);
+    hooks.insert("kernel32.dll!SetFilePointer".into(), hook_SetFilePointer as *const c_void);
+    unsafe { memexec::memexec_exe_with_hooks(&buf, &hooks) }.expect("Failed to execute");
+    Ok(())
+}
+
+/// File that has been read into memory and encoded.
 struct FileHandle {
     data: Vec<u8>,
     pos: u64,
 }
+
+/// Global state shared between hooks.
 #[derive(Default)]
 struct GlobalState {
     cmdline: Option<CString>,
     file_handles: HashMap<isize, FileHandle>,
 }
+
 lazy_static! {
     static ref GLOBAL_STATE: Mutex<GlobalState> = Default::default();
 }
 
-fn get_full_path(path: &CStr) -> Result<CString> {
-    let mut buf = [0u8; 4096];
-    let len =
-        unsafe { GetFullPathNameA(PCSTR(path.as_ptr() as *const u8), Some(buf.as_mut()), None) };
-    if len == 0 {
-        return Err(std::io::Error::last_os_error().into());
-    }
-    Ok(unsafe { CString::from_vec_with_nul_unchecked(buf[..len as usize + 1].to_vec()) })
-}
-
+/// `GetCommandLineA` hook. Skips our own executable name and replaces the subprocess path with an absolute path.
 extern "stdcall" fn hook_GetCommandLineA() -> PCSTR {
     let mut guard = GLOBAL_STATE.lock().expect("Failed to lock global state");
     if let Some(str) = &guard.cmdline {
@@ -122,15 +183,12 @@ extern "stdcall" fn hook_GetCommandLineA() -> PCSTR {
     PCSTR(guard.cmdline.as_ref().unwrap().as_ptr() as *const u8)
 }
 
-fn is_text_file(path: &str) -> bool {
-    path.ends_with(".c")
-        || path.ends_with(".cc")
-        || path.ends_with(".cp")
-        || path.ends_with(".cpp")
-        || path.ends_with(".h")
-        || path.ends_with(".hpp")
+/// `GetCommandLineW` hook. Currently unsupported.
+extern "stdcall" fn hook_GetCommandLineW() -> PCSTR {
+    panic!("GetCommandLineW() is not supported");
 }
 
+/// `CreateFileA` hook. If it's a text file, read it into memory and encode it as Shift-JIS.
 extern "stdcall" fn hook_CreateFileA(
     lpFileName: PCSTR,
     dwDesiredAccess: GENERIC_ACCESS_RIGHTS,
@@ -151,7 +209,7 @@ extern "stdcall" fn hook_CreateFileA(
             hTemplateFile,
         )
     }
-    .unwrap_or(HANDLE(0));
+    .unwrap_or(INVALID_HANDLE_VALUE);
     let err = unsafe { GetLastError() };
 
     let path = unsafe { CStr::from_ptr(lpFileName.as_ptr() as *const c_char) }.to_string_lossy();
@@ -181,16 +239,9 @@ extern "stdcall" fn hook_CreateFileA(
                     match encoded {
                         Cow::Borrowed(_) => {
                             // No modifications were made, use the original data
-                            // println!("READ FILE {:#X}, size {:#X} (UNCHANGED)", ret.0, filesize);
                             guard.file_handles.insert(ret.0, FileHandle { data, pos: 0 });
                         }
                         Cow::Owned(data) => {
-                            println!(
-                                "READ FILE {:#X}, size {:#X} (CHANGED: {:#X})",
-                                ret.0,
-                                filesize,
-                                data.len()
-                            );
                             guard.file_handles.insert(ret.0, FileHandle { data, pos: 0 });
                         }
                     }
@@ -198,42 +249,54 @@ extern "stdcall" fn hook_CreateFileA(
             }
         }
     }
-    // println!("CreateFileA({}, {:#X}) = {:#X}", path, dwDesiredAccess.0, ret.0 as u32);
+    debug_println!("CreateFileA({}, {:#X}) = {:#X}", path, dwDesiredAccess.0, ret.0 as u32);
     unsafe { SetLastError(err) };
     ret
 }
 
+/// `CreateFileW` hook. Currently unsupported.
+extern "stdcall" fn hook_CreateFileW(
+    _lpFileName: PCWSTR,
+    _dwDesiredAccess: GENERIC_ACCESS_RIGHTS,
+    _dwShareMode: FILE_SHARE_MODE,
+    _lpSecurityAttributes: *const SECURITY_ATTRIBUTES,
+    _dwCreationDisposition: FILE_CREATION_DISPOSITION,
+    _dwFlagsAndAttributes: FILE_FLAGS_AND_ATTRIBUTES,
+    _hTemplateFile: HANDLE,
+) -> HANDLE {
+    panic!("CreateFileW() is not supported");
+}
+
+/// `GetFileSize` hook. If the file was read into memory, return that size instead.
 extern "stdcall" fn hook_GetFileSize(hFile: HANDLE, lpFileSizeHigh: *mut u32) -> u32 {
     if !hFile.is_invalid() {
         let guard = GLOBAL_STATE.lock().expect("Failed to lock global state");
         if let Some(file) = guard.file_handles.get(&hFile.0) {
-            // println!("OVERRIDE GetFileSize({:#X}) = {:#X}", hFile.0, file.data.len() as u32);
+            debug_println!("OVERRIDE: GetFileSize({:#X}) = {:#X}", hFile.0, file.data.len() as u32);
             return file.data.len() as u32;
         }
     }
 
     let ret = unsafe { GetFileSize(hFile, Some(lpFileSizeHigh)) };
-    // let err = unsafe { GetLastError() };
-    // println!("GetFileSize({:#X}, {:?}) = {:#X}", hFile.0, lpFileSizeHigh, ret);
-    // unsafe { SetLastError(err) };
+    debug_println!("GetFileSize({:#X}, {:?}) = {:#X}", hFile.0, lpFileSizeHigh, ret);
     ret
 }
 
+/// `CloseHandle` hook. If the file was read into memory, free it.
 extern "stdcall" fn hook_CloseHandle(hObject: HANDLE) -> BOOL {
     if !hObject.is_invalid() {
         let mut guard = GLOBAL_STATE.lock().expect("Failed to lock global state");
         if guard.file_handles.remove(&hObject.0).is_some() {
-            // println!("REMOVED HANDLE {:#X}", hObject.0);
+            debug_println!("File handle removed: {:#X}", hObject.0);
         }
     }
 
     let ret = unsafe { CloseHandle(hObject) };
-    // let err = unsafe { GetLastError() };
-    // println!("CloseHandle({:#X}) = {:#X}", hObject.0, ret.0);
-    // unsafe { SetLastError(err) };
+    debug_println!("CloseHandle({:#X}) = {:#X}", hObject.0, ret.0);
     ret
 }
 
+/// `ReadFile` hook. If the file was read into memory, read from that instead.
 extern "stdcall" fn hook_ReadFile(
     hFile: HANDLE,
     lpBuffer: *mut c_void,
@@ -259,10 +322,14 @@ extern "stdcall" fn hook_ReadFile(
             if !lpNumberOfBytesRead.is_null() {
                 unsafe { *lpNumberOfBytesRead = count };
             }
-            // println!(
-            //     "OVERRIDE ReadFile({:#X}, {:?}, {:#X}, {:?}) = {:#X}",
-            //     hFile.0, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, count
-            // );
+            debug_println!(
+                "OVERRIDE: ReadFile({:#X}, {:?}, {:#X}, {:?}) = {:#X}",
+                hFile.0,
+                lpBuffer,
+                nNumberOfBytesToRead,
+                lpNumberOfBytesRead,
+                count
+            );
             return true.into();
         }
     }
@@ -276,15 +343,18 @@ extern "stdcall" fn hook_ReadFile(
             Some(lpOverlapped),
         )
     };
-    let err = unsafe { GetLastError() };
-    // println!(
-    //     "ReadFile({:#X}, {:?}, {:#X}, {:?}) = {:#X}",
-    //     hFile.0, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, ret.0
-    // );
-    unsafe { SetLastError(err) };
+    debug_println!(
+        "ReadFile({:#X}, {:?}, {:#X}, {:?}) = {:#X}",
+        hFile.0,
+        lpBuffer,
+        nNumberOfBytesToRead,
+        lpNumberOfBytesRead,
+        ret.0
+    );
     ret
 }
 
+/// `SetFilePointer` hook. If the file was read into memory, set the position in that instead.
 extern "stdcall" fn hook_SetFilePointer(
     hFile: HANDLE,
     lDistanceToMove: i32,
@@ -308,9 +378,13 @@ extern "stdcall" fn hook_SetFilePointer(
                 file_size,
             );
             file.pos = pos;
-            println!(
+            debug_println!(
                 "OVERRIDE SetFilePointer({:#X}, {:#X}, {:?}, {}) = {:#X}",
-                hFile.0, distance_to_move, lpDistanceToMoveHigh, dwMoveMethod.0, pos
+                hFile.0,
+                distance_to_move,
+                lpDistanceToMoveHigh,
+                dwMoveMethod.0,
+                pos
             );
             if !lpDistanceToMoveHigh.is_null() {
                 unsafe { *lpDistanceToMoveHigh = (pos >> 32) as i32 };
@@ -321,46 +395,24 @@ extern "stdcall" fn hook_SetFilePointer(
 
     let ret =
         unsafe { SetFilePointer(hFile, lDistanceToMove, Some(lpDistanceToMoveHigh), dwMoveMethod) };
-    let err = unsafe { GetLastError() };
-    // println!(
-    //     "SetFilePointer({:#X}, {:#X}, {:?}, {}) = {:#X}",
-    //     hFile.0, lDistanceToMove, lpDistanceToMoveHigh, dwMoveMethod.0, ret
-    // );
-    unsafe { SetLastError(err) };
+    debug_println!(
+        "SetFilePointer({:#X}, {:#X}, {:?}, {}) = {:#X}",
+        hFile.0,
+        lDistanceToMove,
+        lpDistanceToMoveHigh,
+        dwMoveMethod.0,
+        ret
+    );
     ret
 }
 
-fn main() -> Result<()> {
-    let args: Vec<OsString> = std::env::args_os().collect();
-    if args.len() < 2 {
-        println!("Usage: {} <exe>", args[0].to_string_lossy());
-        exit(1);
+/// Get the absolute path of a file.
+fn get_full_path(path: &CStr) -> Result<CString> {
+    let mut buf = [0u8; 4096];
+    let len =
+        unsafe { GetFullPathNameA(PCSTR(path.as_ptr() as *const u8), Some(buf.as_mut()), None) };
+    if len == 0 {
+        return Err(std::io::Error::last_os_error().into());
     }
-
-    let path = PathBuf::from(&args[1]);
-    let parent = CString::new(
-        path.parent()
-            .expect("Failed to get executable parent directory")
-            .to_string_lossy()
-            .as_ref(),
-    )
-    .unwrap();
-    let parent =
-        get_full_path(&parent).expect("Failed to get absolute executable parent directory");
-    unsafe { SetDllDirectoryA(PCSTR(parent.as_ptr() as *const u8)) }
-        .expect("SetDllDirectoryA() failed");
-    // println!("SetDllDirectoryA({:?})", parent.to_string_lossy());
-
-    let mut buf = Vec::new();
-    File::open(&path).unwrap().read_to_end(&mut buf).unwrap();
-
-    let mut hooks = HashMap::new();
-    hooks.insert("kernel32.dll!GetCommandLineA".into(), hook_GetCommandLineA as *const c_void);
-    hooks.insert("kernel32.dll!CreateFileA".into(), hook_CreateFileA as *const c_void);
-    hooks.insert("kernel32.dll!GetFileSize".into(), hook_GetFileSize as *const c_void);
-    hooks.insert("kernel32.dll!CloseHandle".into(), hook_CloseHandle as *const c_void);
-    hooks.insert("kernel32.dll!ReadFile".into(), hook_ReadFile as *const c_void);
-    hooks.insert("kernel32.dll!SetFilePointer".into(), hook_SetFilePointer as *const c_void);
-    unsafe { memexec::memexec_exe_with_hooks(&buf, &hooks) }.unwrap();
-    Ok(())
+    Ok(unsafe { CString::from_vec_with_nul_unchecked(buf[..len as usize + 1].to_vec()) })
 }
